fb8d350bbdd333f018dab97b49f202575abe9f28
Alison8547 <alisonailson100@hotmail.com>
2021-09-23 19:11:05 -0300

    Aula-06-Gerando-projeto

---
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -0,0 +1,12 @@
+package academy.devdojo.springboot2essentials.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Anime {
+    private String name;
+}
