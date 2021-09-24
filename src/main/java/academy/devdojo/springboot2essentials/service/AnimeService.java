7012a89de6305a0d78bbcfeaa9e978867d466bff
Alison8547 <alisonailson100@hotmail.com>
2021-09-24 11:42:11 -0300

    Aula-08-Método-Get-parte-02

--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -1,13 +1,24 @@
 package academy.devdojo.springboot2essentials.service;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animeList = List.of(new Anime(1L, "Naruto"), new Anime(2L, "HunterxHunter"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1L, "Naruto"), new Anime(2L, "HunterxHunter"));
+        return animeList;
+    }
+
+    public Anime findById(long id) {
+        return animeList.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
