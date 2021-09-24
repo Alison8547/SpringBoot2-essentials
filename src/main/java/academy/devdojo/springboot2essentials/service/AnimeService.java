e9f4e4e32c7ba8765552972554035a11bc85bba8
Alison8547 <alisonailson100@hotmail.com>
2021-09-24 12:07:30 -0300

    Aula-09-Método-Post

--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -2,14 +2,21 @@ package academy.devdojo.springboot2essentials.service;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animeList = List.of(new Anime(1L, "Naruto"), new Anime(2L, "HunterxHunter"));
+    private static List<Anime> animeList;
+
+    static {
+        animeList = new ArrayList<>(List.of(new Anime(1L, "Naruto"), new Anime(2L, "HunterxHunter")));
+    }
 
     public List<Anime> listAll() {
         return animeList;
@@ -20,5 +27,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        animeList.add(anime);
+        return anime;
     }
 }
