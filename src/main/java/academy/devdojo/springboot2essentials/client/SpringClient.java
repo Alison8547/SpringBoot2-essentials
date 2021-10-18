3b21a5d9a56bc0fe353c8a47a5198cb58fb3fb57
Alison8547 <alisonailson100@hotmail.com>
2021-10-18 17:31:23 -0300

    Aula-27-RestTemplate-exchange

--- src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -2,8 +2,13 @@ package academy.devdojo.springboot2essentials.client;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,5 +18,17 @@ public class SpringClient {
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 6);
         log.info(object);
+
+        Anime[] objectArray = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+
+        log.info(Arrays.toString(objectArray));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                });
+
+        log.info(exchange);
     }
 }
