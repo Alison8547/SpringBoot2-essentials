9fc98a736b9c7d0a5634b0e57bd898ff6068fa1d
Alison8547 <alisonailson100@hotmail.com>
2021-10-31 12:00:08 -0300

    Aula-32-Spring-Data-JPA-test-pt03

--- src/test/java/academy/devdojo/springboot2essentials/repository/AnimeRepositoryTest.java
+++ src/test/java/academy/devdojo/springboot2essentials/repository/AnimeRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -80,6 +81,18 @@ class AnimeRepositoryTest {
         Assertions.assertThat(anime).isEmpty();
 
 
+    }
+
+    @Test
+    @DisplayName("Save throw ConstraintViolationException when name is empty")
+    void save_ThrowConstraintViolationExceptionWhenNameIsEmpty() {
+        Anime anime = new Anime();
+//        Assertions.assertThatThrownBy(() -> this.animeRepository.save(anime))
+//                .isInstanceOf(ConstraintViolationException.class);
+
+        Assertions.assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> this.animeRepository.save(anime))
+                .withMessageContaining("The anime name cannot be empty");
     }
 
     private Anime createAnime() {
